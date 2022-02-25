ee24173c183824b0e4c46ec81f13daef298fdca8
Nguyễn Trọng Phúc <ngtrongphuc1905@gmail.com>
2022-02-25 11:37:46 +0700

    Add code

---
+++ js/date.js
@@ -0,0 +1,39 @@
+function timer() {
+    var start = new Date(2021, 8, 09, 00, 00);
+    var t = new Date() - start;
+    var d = Math.floor(t / 1000 / 60 / 60 / 24);
+    var h = Math.floor(t / 1000 / 60 / 60 % 24);
+    if (h < 10) {
+        h = "0" + h;
+    }
+    var m = Math.floor(t / 1000 / 60 % 60);
+    if (m < 10) {
+        m = "0" + m;
+    }
+    var s = Math.floor(t / 1000 % 60);
+    if (s < 10) {
+        s = "0" + s;
+    }
+    document.getElementById("d").innerHTML = d;
+    document.getElementById("h").innerHTML = h;
+    document.getElementById("m").innerHTML = m;
+    document.getElementById("s").innerHTML = s;
+
+}
+
+
+timer();
+setInterval(timer, 1000);
+fadeInterval = setInterval(function () {
+    if (ok == 2) {
+        clearInterval(fadeInterval);
+        fadeinInterval = setInterval(fadein, 50);
+    }
+}, 50)
+
+
+var myMusic = document.getElementById("music");
+
+function playMusic() {
+    myMusic.play();
+}
